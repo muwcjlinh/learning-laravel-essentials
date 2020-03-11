de2aacefa469a21caffa4b20a040e4c40230e4a9
Ryan Nguyen <ryannguyen@ryans-mbp.dsldevice.lan>
2020-03-11 16:14:21 +0700

    index view 3.4

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Booking;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB
+use Illuminate\Support\Facades\DB;
 
 class BookingController extends Controller
 {
