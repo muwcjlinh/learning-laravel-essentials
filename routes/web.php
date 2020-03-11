424bf67fcfb2255380395214d3a750b5444bbd2f
Ryan Nguyen <ryannguyen@ryans-mbp.dsldevice.lan>
2020-03-11 15:31:44 +0700

    Resource controller routes

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/test', function() {return 'Goodbye';});
 
 Route::get('/rooms', 'ShowRoomsController');
+
+Route::resource('bookings', 'BookingController');
